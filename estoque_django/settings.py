fb6544f11610f5fa5e67edfee40275f61c6bb1ca
gregNasc <144745037+gregNasc@users.noreply.github.com>
2026-04-17 12:54:11 -0300

    Update 1.1 settings.py

--- estoque_django/settings.py
+++ estoque_django/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'estoque.apps.EstoqueConfig',
     'core',
+    'estoque',
 ]
 
 MIDDLEWARE = [
@@ -135,10 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
