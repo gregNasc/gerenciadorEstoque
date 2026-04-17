099ee64ddb811d011078926b9bbb54f89131b293
gregNasc <144745037+gregNasc@users.noreply.github.com>
2026-04-17 13:46:55 -0300

    Update 1.0 urls.py

--- estoque_django/urls.py
+++ estoque_django/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('estoque.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('estoque/', include('estoque.urls')),
+    #path('estoque/', include('estoque.urls')),
 ]
 
 if settings.DEBUG:
