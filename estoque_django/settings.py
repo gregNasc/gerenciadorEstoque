f587f427bebac853a9ea8914440a9df96610836f
gregNasc <144745037+gregNasc@users.noreply.github.com>
2026-04-17 13:31:02 -0300

    Update 1.3 settings.py

--- estoque_django/settings.py
+++ estoque_django/settings.py
@@ -137,9 +137,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
